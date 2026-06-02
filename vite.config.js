0dedc09907a403d7d97692f19747dcdae118cb8d
Rodrigo Ormeño <rodrigodanielllanos2@gmail.com>
2026-06-02 01:43:45 -0500

    fix: config base de vite dinamico para desarrollo, vercel y github pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/vendefacil/'
+export default defineConfig(({ command }) => {
+  const isDev = command === 'serve';
+  const isVercel = !!process.env.VERCEL;
+
+  return {
+    plugins: [react()],
+    base: isDev || isVercel ? '/' : '/vendefacil/'
+  }
 })
+
